5717d8b2d8fe9711913e95e33c46061496d5f05c
Agustin <jorgeagustinaparicio@gmail.com>
2026-05-19 22:57:13 -0300

    Fix(front): Update error path

--- frontend/amplix-blog/src/services/auth.services.js
+++ frontend/amplix-blog/src/services/auth.services.js
@@ -8,14 +8,14 @@ import api from "./api";
 export const login = async (credentials) => {
   try {
     const response = await api.post(
-      "./auth/login",
+      "/auth/login",
       credentials
     );  
 
     return response.data;
   } catch (error) {
     throw new Error(
-      error.response?.data?.message ||
+      error.response?.data?.error?.message ||
       "Error al iniciar sesión"
     );
   }
