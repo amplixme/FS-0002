306c5d9c685d8d53f93896da650754f8b6c7b59f
Santiago Emmanuel Molina <santiagoemmanuelmolina@gmail.com>
2026-05-22 12:39:25 -0300

    feat(frontend): implementar AuthContext y proteccion de rutas (Card FS0002-17) (#25)
    * cree AuthContext.jsx
    
    * creacion de ProtectedRoute.jsx
    
    * agregue e importe rutas y proteccion en App de ProtectedRoute y AuthProvider
    
    * refactor: integrar AuthContext en Header para vista dinamica de sesion
    
    * refactor: integrar AuthContext en la pagina de Login
    
    * fix: corregir llamada a la funcion login en componente Login
    
    * fix: corregir llamada a la funcion login en componente Login

--- frontend/amplix-blog/src/App.jsx
+++ frontend/amplix-blog/src/App.jsx
@@ -1,27 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { AuthProvider } from "./context/AuthContext";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Layout>
-              <Home />
-            </Layout>
-          }
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />        
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Layout>
+                <Home />
+              </Layout>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
